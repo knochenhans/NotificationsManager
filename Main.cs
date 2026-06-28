2ab9ef0fdab3864754be68453eb7415c33416643
Andre Jonas <nipsky@googlemail.com>
2026-06-28 11:45:38 +0200

    Updated example

--- Main.cs
+++ Main.cs
@@ -8,11 +8,17 @@ public partial class Main : Node2D
 
     public override async void _Ready()
     {
-        await Task.Delay(2000);
+        await Task.Delay(1000);
         GD.Print("Showing Notification 1");
-        NotificationManager.ShowNotification("Hello World! 1", 10);
-        await Task.Delay(2000);
+        NotificationManager.ShowNotification("Hello World! 1", 2);
+        await Task.Delay(500);
         GD.Print("Showing Notification 2");
-        NotificationManager.ShowNotification("Hello World! 2", 10);
+        NotificationManager.ShowNotification("Hello World! 2", 2);
+        await Task.Delay(500);
+        GD.Print("Showing Notification 3");
+        NotificationManager.ShowNotification("Hello World! 3", 2);
+        await Task.Delay(2000);
+        GD.Print("Showing Notification 4");
+        NotificationManager.ShowNotification("Hello World! 4", 2);
     }
 }
